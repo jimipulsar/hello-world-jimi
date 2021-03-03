1cfec472338d29ae4fa6c121647ec07e47b523ad
Jimi Pulsar <jimipulsar@gmail.com>
2021-03-03 22:11:21 +0100

    updated

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'BaseController@index')->name('index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Auth::routes();
 
