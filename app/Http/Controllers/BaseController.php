b397147a5d9e695492b324ad19557f1a88ba4935
Jimi Pulsar <jimipulsar@gmail.com>
2021-03-03 22:41:43 +0100

    updated user list

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -7,7 +7,12 @@ use Illuminate\Http\Request;
 class BaseController extends Controller
 {
   public function index() {
+
+      return view('layouts.app');
+  }
+
+  public function lista() {
       $users = User::orderBy('id','ASC')->paginate(5);
-      return view('layouts.app', compact('users'));
+      return view('home', compact('users'));
   }
 }
