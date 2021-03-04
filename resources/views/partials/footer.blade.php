a35e2ddf91e892d5707192537960a2553ba13cda
Jimi Pulsar <jimipulsar@gmail.com>
2021-03-04 16:23:19 +0100

    footer added on pages

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,12 @@
+<!--Footer-->
+<footer class="bg-light text-lg-start">
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-image: linear-gradient(140deg, #FFF 0%, #D2ECFB 50%, #78C0FF 75%);">
+    Developed by Pietro Ricci
+    <a class="nav-link" href="https://github.com/jimipulsar?tab=repositories" rel="nofollow" target="_blank">
+      <i class="fab fa-github"></i> Github
+    </a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!--Footer-->
