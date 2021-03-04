f2b319d0b6a89f5ba425e36ff45d0adf358d30eb
Jimi Pulsar <jimipulsar@gmail.com>
2021-03-04 15:23:59 +0100

    updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,44 +6,12 @@
 <main class="mt-5" id="crud">
 <div class="container">
   <!--Section: Content-->
-  <section>
-    <div class="row">
-      <div class="col-md-6 gx-5 mb-4">
-        <div class="bg-image hover-overlay ripple shadow-2-strong" data-mdb-ripple-color="light">
-          <img src="https://mdbootstrap.com/img/new/slides/031.jpg" class="img-fluid" />
-          <a href="#!">
-            <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
-          </a>
-        </div>
-      </div>
-
-      <div class="col-md-6 gx-5 mb-4">
-        <h4><strong>Facilis consequatur eligendi</strong></h4>
-        <p class="text-muted">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis consequatur
-          eligendi quisquam doloremque vero ex debitis veritatis placeat unde animi laborum
-          sapiente illo possimus, commodi dignissimos obcaecati illum maiores corporis.
-        </p>
-        <p><strong>Doloremque vero ex debitis veritatis?</strong></p>
-        <p class="text-muted">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod itaque voluptate
-          nesciunt laborum incidunt. Officia, quam consectetur. Earum eligendi aliquam illum
-          alias, unde optio accusantium soluta, iusto molestiae adipisci et?
-        </p>
-      </div>
-    </div>
-  </section>
-  <!--Section: Content-->
-
-  <hr class="my-5" />
-
-  <!--Section: Content-->
   <section class="text-center">
     <h4 class="mb-5"><strong>Lista Utenti</strong></h4>
 
     <div class="row">
+        @foreach($users as $user)
       <div class="col-lg-4 col-md-12 mb-4">
-      @foreach($users as $user)
         <div class="card">
           <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
             <img
@@ -64,9 +32,8 @@
           </div>
 
         </div>
-          @endforeach
       </div>
-
+        @endforeach
     </div>
     {{$users->links()}}
   </section>
